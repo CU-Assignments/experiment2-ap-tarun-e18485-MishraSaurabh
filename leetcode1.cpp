b0b4f32e0843ec783bc39116507b4812c6f316cd
Saurabh Mishra <158066934+MishraSaurabh@users.noreply.github.com>
2025-02-17 21:57:19 +0530

    Create leetcode1.cpp

---
+++ leetcode1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> d;
+        for (int i = 0;; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (d.contains(y)) {
+                return {d[y], i};
+            }
+            d[x] = i;
+        }
+    }
+};
+
+/*OUTPUT: 
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]*/
